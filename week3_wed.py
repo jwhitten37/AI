dc05b9881d927604dc986ad3bcb2ba3e36db40ca
Justin <jut_cut_37@yahoo.com>
2024-08-05 10:22:01 -0400

    committed

--- week3_wed.py
+++ week3_wed.py
@@ -1,5 +1,9 @@
-import numpy as np
 
+d = {
+    "Annie": [67,90,87],
+    "Bryan": [75,72,81],
+    "Charlie": [82,100,91]
+}
 
 def averageStudents(filename, gradesDict):
     try:
@@ -7,10 +11,11 @@ def averageStudents(filename, gradesDict):
     except FileNotFoundError:
         open(filename, "w")
     
-    with open(filename,'r') as file:
-        w = open(filename,'a')
-        w.write(context)
-        for line in file.readlines():
-            print(line)
+    with open(filename,'a') as file:
+        for g in gradesDict:
+            average = round(sum(num for num in d[g]) / len(d[g]))
+            file.write("NAME: " + g + " GRADES: " + str(d[g][0]) + " " + str(d[g][1]) + " " + str(d[g][2]) + " AVERAGE: " + str(average) + "\n")
+        print(file)
+            
     
-func("this.txt","hello\n")
+averageStudents("students.txt",d)
