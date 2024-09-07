4c10406801df317c125d054009c32508afa1121f
Jut <jut_cut_37@yahoo.com>
2024-09-07 04:10:46 -0400

    change

--- practice.py
+++
@@ -1,7 +0,0 @@
-ms = """Identify, synchronize, and accelerate the development, integration, and adoption of effective and timely AI solutions."""
-
-print(ms.lower())
-print(ms.upper())
-print(ms.title())
-print(ms.replace(',','!'))
-print(ms.split('and'))
